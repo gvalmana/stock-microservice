50a5e984636ff1197029fd317166ece11237bb91
Gustavo Valmaña <gustavo.shotokai@gmail.com>
2024-02-12 19:40:21 -0500

    agregados el dev

--- app/Http/Controllers/ReciveOrderController.php
+++ app/Http/Controllers/ReciveOrderController.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Log;
 /**
 * @OA\Info(title="API Stock Microservice", version="1.0")
 *
-* @OA\Server(url="http://localhost:8001")
+* @OA\Server(url="http://95.183.53.25:8001")
 */
 class ReciveOrderController extends Controller
 {
